01b11f890dbd26f70d781c73429a19b8c2a20105
DerBot7214 <lengwenath1@gmail.com>
2023-05-22 12:13:31 +0200

    unused import

--- app/sql_app/models.py
+++ app/sql_app/models.py
@@ -1,5 +1,5 @@
 from typing import List
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import ForeignKey, Integer, String
 from sqlalchemy.orm import relationship, Mapped, mapped_column
 
 from .database import Base
